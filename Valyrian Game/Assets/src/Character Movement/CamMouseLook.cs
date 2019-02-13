7b1afbbbc1f5e2e8d053cb01da882cd78f679b9e
Jose Sotolongo <infamousclarkg@gmail.com>
2019-02-13 11:18:55 -0500

    VN-4 Added movement to the look of the FP

---
+++ Valyrian Game/Assets/src/Character Movement/CamMouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMouseLook : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    public float sensitivity = 5.0F;
+    public float smoothing = 2.0F;
+
+    GameObject character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
