abe43dbd8891d5e56022aca7de6a0f84dd564b70
Ozzie Fernandez <ozfernandez@knights.ucf.edu>
2019-03-14 16:49:47 -0400

    VN-13, VN-14, VN-15, VN-16. Added Main screen, CreateGame screen, and JoinGame screen; along with full button functionality.

---
+++ Valyrian Game/Assets/createTitleSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createTitleSwitch : MonoBehaviour
+{
+    GameObject titleCamera;
+    GameObject createGameCamera;
+    // Start is called before the first frame update
+    public void onClick()
+    {
+        titleCamera.SetActive(false);
+        createGameCamera.SetActive(true);
+    }
+}
