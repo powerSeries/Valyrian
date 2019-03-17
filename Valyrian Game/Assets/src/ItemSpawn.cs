6dbd36404915e173c634bbce8884bd2d961e6650
Jose Sotolongo <joseesotolongo@gmail.com>
2019-03-17 11:39:29 -0400

    VN-7 Created item spawning system. Placed spawn points of the items

---
+++ Valyrian Game/Assets/src/ItemSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawn : MonoBehaviour
+{
+    //Serialize the gameobject in order to create more of the object
+    [SerializeField]
+    private GameObject itemPrefab;
+
+    private void Start()
+    {
+        //Create a new gameobject of what itemPrefab is, give it the rotation and position of current object
+        Instantiate(itemPrefab, transform.position, transform.rotation);
+    }
+}
