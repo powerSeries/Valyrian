2bd6ce4d1bcdf246b7ac69ec24042d5e4c494101
Jose Sotolongo <joseesotolongo@gmail.com>
2019-03-17 12:08:10 -0400

    VN-17 Added the ability to create a level and load in

---
+++ Valyrian Game/Assets/src/UI/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    private int sceneNum;
+
+    public void ChangeScene(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
