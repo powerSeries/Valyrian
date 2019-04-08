3e8f9f3ff2e63ed9d6ad86c75e7b0a886805019d
hunterkiddo <hunter.kiddo@hotmail.com>
2019-04-08 07:42:03 -0400

    VN-32, VN-33, VN-34+
    Various gun additions and improvements including animation, recoil, damage, etc

---
+++ Valyrian Game/Assets/src/Shooting/AutoGunFire.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AutoGunFire : MonoBehaviour
+{
+
+    public int gunDamage = 1;
+    public float fireRate = .5f;
+    public float weaponRange = 5f;
+    public float hitForce = 100f;
+    public int recoil = 0;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private WaitForSeconds sightsDuration = new WaitForSeconds(.15f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    public bool aiming = false;
+    private float nextFire;
+
+    void Start()
+    {
+        InitializeComponents();
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void InitializeComponents()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    private void CheckInput()
+    {
+        if (PressedAimButton())
+        {
+            AimDownSights();
+        }
+        else if (!PressingAimButton() && aiming)
+        {
+            CancelSights();
+        }
+        else
+        {
+            CheckIfFiring();
+        }
+    }
+
+    public void CheckIfFiring()
+    {
+        if (PressingFireButton() && TimeToShoot() && PressingAimButton())
+        {
+            FireGunWhileAiming();
+        }
+        else if (PressingFireButton() && TimeToShoot())
+        {
+            FireGunWithoutAiming();
+        }
+    }
+
+    private void FireGunWhileAiming()
+    {
+        SetNextFireTime();
+
+        StartCoroutine(ShotEffectSights());
+
+        RayCastDetection();
+    }
+
+    private void FireGunWithoutAiming()
+    {
+        SetNextFireTime();
+        StartCoroutine(ShotEffect());
+        RayCastDetection();
+    }
+
+    private void RayCastDetection()
+    {
+        Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        laserLine.SetPosition(0, gunEnd.position);
+
+        if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+        {
+            laserLine.SetPosition(1, hit.point);
+        }
+        else
+        {
+            laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+        }
+
+    }
+
+    public bool PressingFireButton()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool PressedAimButton()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool PressingAimButton()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool TimeToShoot()
+    {
+        if (Time.time > nextFire)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void AimDownSights()
+    {
+        aiming = true;
+        GetComponent<Animation>().Play("AimDownSights");
+    }
+
+    private void CancelSights()
+    {
+        aiming = false;
+        GetComponent<Animation>().Play("CancelSights");
+    }
+
+    private void SetNextFireTime()
+    {
+        nextFire = Time.time + fireRate;
+    }
+
+    private void ApplyRecoil()
+    {
+        //Player Object Variant Rotation.x += recoil;
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+
+        laserLine.enabled = true;
+        ApplyRecoil();
+        GetComponent<Animation>().Play("Recoil");
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+
+    private IEnumerator ShotEffectSights()
+    {
+        gunAudio.Play();
+
+        laserLine.enabled = true;
+        ApplyRecoil();
+        GetComponent<Animation>().Play("RecoilSights");
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
---
+++ Valyrian Game/Assets/src/Shooting/AutoReload.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoReload : MonoBehaviour
+{
+    public AudioSource ReloadSound;
+    public GameObject Reticle;
+    //public GameObject MechanicsObject;
+    public int ClipSize = 10;
+    public int ClipCount;
+    public int TotalCount;
+    public int ReloadAvailable;
+    public AutoGunFire GunObject;
+    public CharacterVitality CharacterObject;
+
+    void Start()
+    {
+        InitiateObjects();
+    }
+
+    void Update()
+    {
+        GetAmmoCounts();
+        CheckIfReloadAvailable();
+        CheckForReload();
+    }
+
+    private void InitiateObjects()
+    {
+        GunObject = GetComponent<AutoGunFire>();
+        CharacterObject = GetComponent<CharacterVitality>();
+    }
+
+    private void GetAmmoCounts()
+    {
+        ClipCount = CharacterObject.LoadedAmmoCount;
+        TotalCount = CharacterObject.TotalAmmoCount;
+    }
+
+    private void CheckIfReloadAvailable()
+    {
+        if (TotalCount == 0)
+        {
+            ReloadAvailable = 0;
+        }
+        else
+        {
+            ReloadAvailable = ClipSize - ClipCount;
+        }
+    }
+
+    private void CheckForReload()
+    {
+        if (PressedReloadButton())
+        {
+            if (AnyReloadAvailable())
+            {
+                CheckAvailableAmmo();
+            }
+            StartCoroutine(EnableScripts());
+        }
+    }
+
+    private void CheckAvailableAmmo()
+    {
+        if (TotalCount <= ReloadAvailable)
+        {
+            LastReload();
+        }
+        else
+        {
+            AnotherReload();
+        }
+        ActionReload();
+    }
+
+    private void LastReload()
+    {
+        CharacterObject.LoadedAmmoCount += TotalCount;
+        CharacterObject.TotalAmmoCount -= TotalCount;
+    }
+
+    private void AnotherReload()
+    {
+        CharacterObject.LoadedAmmoCount += ReloadAvailable;
+        CharacterObject.TotalAmmoCount -= ReloadAvailable;
+    }
+
+    private bool PressedReloadButton()
+    {
+        if (Input.GetButtonDown("Reload"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool AnyReloadAvailable()
+    {
+        if (ReloadAvailable >= 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator EnableScripts()
+    {
+        yield return new WaitForSeconds(1.1f);
+        GunObject.enabled = true;
+        Reticle.SetActive(true);
+        //MechanicsObject.SetActive(true);
+    }
+
+    void ActionReload()
+    {
+        GunObject.enabled = false;
+        Reticle.SetActive(false);
+        //MechanicsObject.SetActive(false);
+        ReloadSound.Play();
+        GetComponent<Animation>().Play("Reload");
+    }
+}
